f2afb89047c9186778d24167345d771feae0a435
daehoon0409 <wwwbjacau85@gmail.com>
2024-05-17 14:23:08 +0900

    [Feat] 함치기 까지 진행

--- UnityProject_24_1_B/Assets/Scripts/Game/CircleObject.cs
+++ UnityProject_24_1_B/Assets/Scripts/Game/CircleObject.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CircleObject : MonoBehaviour
 {
-    public bool isDrag;
+    public bool isDrag;      
     public bool isUsed;
     Rigidbody2D rigidbody2D;
 
-    void Start()
+    public int index;
+
+    void Awake()
     {
         isUsed = false;
         rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.simulated = false;
+    }
+
+    void Start()
+    {
+     
     }
 
     void Update()
@@ -53,6 +61,39 @@ public class CircleObject : MonoBehaviour
         if(Temp != null)
         {
             Temp.gameObject.GetComponent<GameManager>().GenObject();
+        }
+    }
+
+    public void Used()
+    {
+        isDrag = false;                   //드래그가 종료
+        isUsed = true;                    //사용이 완료
+        rigidbody2D.simulated = true;      //물리 현상 시작
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)   //2D 충돌이 일어날경우
+    {
+        if (index >= 7)                //준비된 과일이 최대 7개
+            return;
+        if (collision.gameObject.tag == "Fruit")  //충돌 물체의 TAG가 Fruit 일 경우
+        {
+            CircleObject temp = collision.gameObject.GetComponent<CircleObject>();  //임시로 Class temp를 선언하고 충돌체의 Class를받아온다
+
+            if(temp.index == index)  //과일 번호가 같은경우
+            {
+                if (gameObject.GetInstanceID() > collision.gameObject.GetInstanceID())  //유니티에서 지원하는 고유의 ID를받아와서 ID가 큰쪽에서 다음 과일 생성
+                {
+                    //gameManger 에서 생성함수 호출
+                    GameObject Temp = GameObject.FindWithTag("GameManager");
+                    if (Temp != null)
+                    {
+                        Temp.gameObject.GetComponent<GameManager>().MergeObject(index , gameObject.transform.position); //생성된 MerageObject 함수에 인수와 함께 전달
+                    }
+                    
+                    Destroy(temp.gameObject);          //충돌 물체 파괴
+                    Destroy(gameObject);              //자기 자신 파괴
+                }
+            }
         }
     }
 }
--- UnityProject_24_1_B/Assets/Scripts/Game/GameManager.cs
+++ UnityProject_24_1_B/Assets/Scripts/Game/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject CircleObject;
+    public GameObject[] CircleObject;
     public Transform GenTransform;
     public float TimeCheck;
     public bool isGen;
@@ -22,7 +22,8 @@ public class GameManager : MonoBehaviour
             TimeCheck -= Time.deltaTime;
             if(TimeCheck <= 0 )
             {
-                GameObject Temp = Instantiate(CircleObject);
+                int RandNumber = Random.Range(0, 3);
+                GameObject Temp = Instantiate(CircleObject[0]);
                 Temp.transform.position = GenTransform.position;
                 isGen = true;
             }
@@ -32,5 +33,12 @@ public class GameManager : MonoBehaviour
     {
         isGen = false;
         TimeCheck = 1.0f;
+    }
+
+    public void MergeObject(int index, Vector3 position)   //Merge 함수는 과일번호(int) 과 생성 위치값(Vector3)을 전달 받는다
+    {
+        GameObject Temp = Instantiate(CircleObject[index]); //index를 그대로 쓴다
+        Temp.transform.position = position;                 //위치는 전달 받은 값으로 사용
+        Temp.GetComponent<CircleObject>().Used();
     }
 }
